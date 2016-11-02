1b4a7abb0c24ef636c6f6ddf9d0a3c8098f4b65b
ibrahim <ibr.12399@gmail.com>
2016-11-02 19:00:42 +0400

    less time btn added

--- app/src/main/java/com/example/rasheedmenem/time/MainActivity.java
+++ app/src/main/java/com/example/rasheedmenem/time/MainActivity.java
@@ -137,6 +137,20 @@ public class MainActivity extends AppCompatActivity {
                       }
                 });
 
+        less.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                t.cancel();
+                long x = (voidcall.Time_call * 3600000) + (voidcall.min_call * 60000) - 60000;
+
+                // put x in timer
+                timer.setText(String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(x),
+                        TimeUnit.MILLISECONDS.toMinutes(x) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(x)),
+                        TimeUnit.MILLISECONDS.toSeconds(x) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(x))));
+                t = new timer(x , 1000);
+                t.start();
+            }
+        });
+
         //==============================================================================================
 
 
